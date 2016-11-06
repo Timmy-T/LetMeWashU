3f7c9993387c5dd4c4c7a40306923f8515acca4f
Timothy Buesking <Timothy Buesking>
2016-11-06 01:48:57 -0500

    Merge finished

--- initMap.js
+++ initMap.js
@@ -201,13 +201,9 @@
 
                   google.maps.event.addListener(marker, 'click', function() {
                       infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-<<<<<<< HEAD
-                          'Distance: ' + distance.toFixed(2) + 'm <br> Calories burned walking: ' + calories.toFixed(2) + ' cal* <br>' +
-                          place.formatted_address + '<br><a href="#"><b>Get Directions!</b></a></div>');
-=======
+
                           'Distance: ' + distance.toFixed(2) + 'm <br> Calories burned walking*: ' + calories.toFixed(2) + ' cal <br>' +
                           place.formatted_address + '<br><a target="_blank" href="' + directionsUrl + '"><b>Get Directions!</b></a></div>');
->>>>>>> origin/master
 
                       console.log(place.name);
                       $.post("phpFoodQuery.php", {
